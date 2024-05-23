70ac1eeecb69f0128c39d46b03d162b39bbcd279
Socheat Sok <socheatsok78@gmail.com>
2024-05-24 01:05:56 +0700

    Convert syntax indentation to tab

--- rootfs/docker-entrypoint-shim.sh
+++ rootfs/docker-entrypoint-shim.sh
@@ -6,12 +6,12 @@
 # VAULT_*_ADDR is also set, the resulting URI will combine the protocol and port
 # number with the IP of the named interface.
 get_addr () {
-    local if_name=$1
-    local uri_template=$2
-    ip addr show dev $if_name | awk -v uri=$uri_template '/\s*inet\s/ { \
-      ip=gensub(/(.+)\/.+/, "\\1", "g", $2); \
-      print gensub(/^(.+:\/\/).+(:.+)$/, "\\1" ip "\\2", "g", uri); \
-      exit}'
+	local if_name=$1
+	local uri_template=$2
+	ip addr show dev $if_name | awk -v uri=$uri_template '/\s*inet\s/ { \
+	  ip=gensub(/(.+)\/.+/, "\\1", "g", $2); \
+	  print gensub(/^(.+:\/\/).+(:.+)$/, "\\1" ip "\\2", "g", uri); \
+	  exit}'
 }
 
 # VAULT_DATA_DIR is exposed as a volume for possible persistent storage. The
@@ -26,45 +26,45 @@ VAULT_STORAGE_CONFIG_FILE=${VAULT_STORAGE_CONFIG_FILE:-"$VAULT_CONFIG_DIR/raft-s
 # Specifies the address (full URL) to advertise to other
 # Vault servers in the cluster for client redirection.
 if [ -n "$VAULT_API_INTERFACE" ]; then
-    export VAULT_API_ADDR=$(get_addr $VAULT_API_INTERFACE ${VAULT_API_ADDR:-"https://0.0.0.0:8200"})
-    echo "Using $VAULT_API_INTERFACE for VAULT_API_ADDR: $VAULT_API_ADDR"
+	export VAULT_API_ADDR=$(get_addr $VAULT_API_INTERFACE ${VAULT_API_ADDR:-"https://0.0.0.0:8200"})
+	echo "Using $VAULT_API_INTERFACE for VAULT_API_ADDR: $VAULT_API_ADDR"
 fi
 
 # Configure the Vault API address for CLI usage
 if [[ -n "${VAULT_API_ADDR}" ]]; then
-    export VAULT_ADDR=${VAULT_API_ADDR}
+	export VAULT_ADDR=${VAULT_API_ADDR}
 elif [[ -n "${VAULT_REDIRECT_ADDR}" ]]; then
-    export VAULT_ADDR=${VAULT_REDIRECT_ADDR}
+	export VAULT_ADDR=${VAULT_REDIRECT_ADDR}
 elif [[ -n "${VAULT_ADVERTISE_ADDR}" ]]; then
-    export VAULT_ADDR=${VAULT_ADVERTISE_ADDR}
+	export VAULT_ADDR=${VAULT_ADVERTISE_ADDR}
 fi
 
 # Integrated storage (Raft) backend
 if [[ -n "${VAULT_RAFT_NODE_ID}" ]]; then
-    echo "Configure VAULT_RAFT_NODE_ID as \"$VAULT_RAFT_NODE_ID\""
-    export VAULT_RAFT_NODE_ID=${VAULT_RAFT_NODE_ID}
+	echo "Configure VAULT_RAFT_NODE_ID as \"$VAULT_RAFT_NODE_ID\""
+	export VAULT_RAFT_NODE_ID=${VAULT_RAFT_NODE_ID}
 fi
 export VAULT_RAFT_PATH=${VAULT_RAFT_PATH:-"/vault/file"}
 echo "Configure VAULT_RAFT_PATH to \"$VAULT_RAFT_PATH\""
 
 # If VAULT_STORAGE_CONFIG_FILE doesn't exist, generate a default "raft" storage configuration
 if [ ! -f "$VAULT_STORAGE_CONFIG_FILE" ]; then
-    # Vault Cloud Auto Join
-    if [[ -n "${VAULT_CLOUD_AUTO_JOIN}" ]]; then
-        VAULT_CLOUD_AUTO_JOIN_SCHEME=${VAULT_CLOUD_AUTO_JOIN_SCHEME:-"https"}
-        VAULT_CLOUD_AUTO_JOIN_PORT=${VAULT_CLOUD_AUTO_JOIN_PORT:-"8201"}
-        echo "storage \"raft\" { retry_join { auto_join_scheme=\"${VAULT_CLOUD_AUTO_JOIN_SCHEME}\" auto_join_port=${VAULT_CLOUD_AUTO_JOIN_PORT} auto_join=\"${VAULT_CLOUD_AUTO_JOIN}\" } }" > "$VAULT_STORAGE_CONFIG_FILE"
-    fi
+	# Vault Cloud Auto Join
+	if [[ -n "${VAULT_CLOUD_AUTO_JOIN}" ]]; then
+		VAULT_CLOUD_AUTO_JOIN_SCHEME=${VAULT_CLOUD_AUTO_JOIN_SCHEME:-"https"}
+		VAULT_CLOUD_AUTO_JOIN_PORT=${VAULT_CLOUD_AUTO_JOIN_PORT:-"8201"}
+		echo "storage \"raft\" { retry_join { auto_join_scheme=\"${VAULT_CLOUD_AUTO_JOIN_SCHEME}\" auto_join_port=${VAULT_CLOUD_AUTO_JOIN_PORT} auto_join=\"${VAULT_CLOUD_AUTO_JOIN}\" } }" > "$VAULT_STORAGE_CONFIG_FILE"
+	fi
 
-    # Write the listener configuration to the file
-    echo "storage \"raft\" { /* default config */ }" > "$VAULT_STORAGE_CONFIG_FILE"
+	# Write the listener configuration to the file
+	echo "storage \"raft\" { /* default config */ }" > "$VAULT_STORAGE_CONFIG_FILE"
 fi
 
 # If VAULT_SEAL_TYPE is set to "transit", configure the transit seal
 # https://developer.hashicorp.com/vault/docs/configuration/seal/transit
 if [[ -n "$VAULT_SEAL_SECRET_FILE" ]] && [ -f "$VAULT_SEAL_SECRET_FILE" ]; then
-    echo "==> The Transit seal configuration is provided by the file \"$VAULT_SEAL_SECRET_FILE\", activating the Transit seal..."
-    cp "$VAULT_SEAL_SECRET_FILE" "$VAULT_CONFIG_DIR"
+	echo "==> The Transit seal configuration is provided by the file \"$VAULT_SEAL_SECRET_FILE\", activating the Transit seal..."
+	cp "$VAULT_SEAL_SECRET_FILE" "$VAULT_CONFIG_DIR"
 fi
 
 # Specifies the identifier for the Vault cluster.
@@ -91,16 +91,16 @@ VAULT_DEFAULT_MAX_REQUEST_DURATION=${VAULT_DEFAULT_MAX_REQUEST_DURATION:-"0"}
 # Raw storage endpoint configuration
 VAULT_RAW_STORAGE_ENDPOINT=${VAULT_RAW_STORAGE_ENDPOINT:-"false"}
 if [[ "${VAULT_RAW_STORAGE_ENDPOINT}" == "true" ]]; then
-    echo ""
-    echo "----------------------------------------------------------------------"
-    echo "                            !!! WARNING !!!                           "
-    echo "----------------------------------------------------------------------"
-    echo "Vault is configured to use the raw storage endpoint. This is a highly"
-    echo "privileged endpoint"
-    echo ""
-    echo "Enables the sys/raw endpoint which allows the decryption/encryption"
-    echo "of raw data into and out of the security barrier."
-    echo "----------------------------------------------------------------------"
+	echo ""
+	echo "----------------------------------------------------------------------"
+	echo "                            !!! WARNING !!!                           "
+	echo "----------------------------------------------------------------------"
+	echo "Vault is configured to use the raw storage endpoint. This is a highly"
+	echo "privileged endpoint"
+	echo ""
+	echo "Enables the sys/raw endpoint which allows the decryption/encryption"
+	echo "of raw data into and out of the security barrier."
+	echo "----------------------------------------------------------------------"
 fi
 
 # Save the configuration to a file
@@ -135,14 +135,14 @@ listener "tcp" {
   tls_cert_file = "${VAULT_LISTENER_TLS_CERT_FILE}"
   tls_key_file = "${VAULT_LISTENER_TLS_KEY_FILE}"
   telemetry {
-    unauthenticated_metrics_access = true
+	unauthenticated_metrics_access = true
   }
 }
 
 # Prometheus metrics
 telemetry {
-    prometheus_retention_time = "24h"
-    disable_hostname = true
+	prometheus_retention_time = "24h"
+	disable_hostname = true
 }
 EOT
 
--- rootfs/dockerswarm-entrypoint.sh
+++ rootfs/dockerswarm-entrypoint.sh
@@ -3,96 +3,96 @@
 
 # Get the IP addresses of the tasks of the service using DNS resolution
 function dockerswarm_sd() {
-    local service_name=$1
-    if [ -z "$service_name" ]; then
-        echo "[dockerswarm_sd]: command line is not complete, service name is required"
-        return 1
-    fi
-    dig +short "tasks.${service_name}" | sort
+	local service_name=$1
+	if [ -z "$service_name" ]; then
+		echo "[dockerswarm_sd]: command line is not complete, service name is required"
+		return 1
+	fi
+	dig +short "tasks.${service_name}" | sort
 }
 
 # Get IP address using the Docker service network name instead of interface name
 function dockerswarm_network_addr() {
-    local network_name=$1
-    if [ -z "$network_name" ]; then
-        echo "[dockerswarm_network_addr]: command line is not complete, network name is required"
-        return 1
-    fi
-    # Loop through assigned IP addresses to the host
-    for ip in $(hostname -i); do
-        # Query the PTR record for the IP address
-        local ptr_record=$(host "$ip" | cut -d ' ' -f 5)
-        # If the PTR record is empty, skip to the next IP address
-        if [ -z "$ptr_record" ]; then
-            continue
-        fi
-        # Filter the PTR record to get the network name
-        local service_network=$(echo "$ptr_record" | cut -d '.' -f 4)
-        # Check if the network name matches the input network name
-        if [[ "$service_network" == *"$network_name" ]]; then
-            echo "$ip"
-            return
-        fi
-    done
+	local network_name=$1
+	if [ -z "$network_name" ]; then
+		echo "[dockerswarm_network_addr]: command line is not complete, network name is required"
+		return 1
+	fi
+	# Loop through assigned IP addresses to the host
+	for ip in $(hostname -i); do
+		# Query the PTR record for the IP address
+		local ptr_record=$(host "$ip" | cut -d ' ' -f 5)
+		# If the PTR record is empty, skip to the next IP address
+		if [ -z "$ptr_record" ]; then
+			continue
+		fi
+		# Filter the PTR record to get the network name
+		local service_network=$(echo "$ptr_record" | cut -d '.' -f 4)
+		# Check if the network name matches the input network name
+		if [[ "$service_network" == *"$network_name" ]]; then
+			echo "$ip"
+			return
+		fi
+	done
 
-    echo "[dockerswarm_network_addr]: can't find network '$network_name'"
-    return 2
+	echo "[dockerswarm_network_addr]: can't find network '$network_name'"
+	return 2
 }
 
 # A custom implementation for get_addr from official Vault image
 function dockerswarm_get_addr() {
-    local if_name=$1
-    local uri_template=$2
-    dockerswarm_network_addr $if_name | awk -v uri=$uri_template '{print gensub(/^(.+:\/\/).+(:.+)$/, "\\1" $1 "\\2", "g", uri)}'
+	local if_name=$1
+	local uri_template=$2
+	dockerswarm_network_addr $if_name | awk -v uri=$uri_template '{print gensub(/^(.+:\/\/).+(:.+)$/, "\\1" $1 "\\2", "g", uri)}'
 }
 
 # Docker Swarm Auto Join for Hashicorp Vault
 function dockerswarm_auto_join() {
-    local auto_join_scheme=${DOCKERSWARM_AUTO_JOIN_SCHEME:-"http"}
-    local auto_join_port=${DOCKERSWARM_AUTO_JOIN_PORT:-"8200"}
+	local auto_join_scheme=${DOCKERSWARM_AUTO_JOIN_SCHEME:-"http"}
+	local auto_join_port=${DOCKERSWARM_AUTO_JOIN_PORT:-"8200"}
 
-    # Loop to check the tasks of the service
-    local current_cluster_ips=""
-    while true; do
-        local auto_join_config=""
-        local cluster_ips=$(dockerswarm_sd "${1}")
-        # Skip if the cluster_ips is empty
-        if [[ -z "${cluster_ips}" ]]; then
-            current_cluster_ips="" # reset the current_cluster_ips
-            continue
-        fi
-        # if cluster_ips only contains one IP address, then it's a single node cluster
-        if [[ $(echo "${cluster_ips}" | wc -l) -eq 1 ]]; then
-            # Write the configuration to the file
-            echo "storage \"raft\" { /* single node cluster */ }" > "$VAULT_STORAGE_CONFIG_FILE"
-            continue
-        fi
-        # Check if the current_cluster_ips is different from the cluster_ips
-        if [[ "${current_cluster_ips}" != "${cluster_ips}" ]]; then
-            # Update the current_cluster_ips
-            current_cluster_ips=$cluster_ips
-            # Loop to add the tasks to the auto_join_config
-            for task in ${cluster_ips}; do
-                # Skip if the task is the current node
-                if [[ "$(hostname -i)" == *"${task}"* ]]; then
-                    continue
-                fi
-                # Add the task to the auto_join_config
-                if [[ -n "${auto_join_config}" ]]; then
-                    auto_join_config="${auto_join_config} "
-                fi
-                auto_join_config="${auto_join_config}retry_join { leader_api_addr = \"${auto_join_scheme}://${task}:${auto_join_port}\" }"
-            done
-            # Write the configuration to the file
-            echo "storage \"raft\" { ${auto_join_config} }" > "$VAULT_STORAGE_CONFIG_FILE"
-            # Send a SIGHUP signal to reload the configuration
-            if [ -f "$VAULT_PID_FILE" ]; then
-                echo "==> [Docker Swarm Entrypoint] detected a change in the cluster, reloading the configuration..."
-                kill -s SIGHUP $(cat $VAULT_PID_FILE)
-            fi
-        fi
-        sleep 15
-    done
+	# Loop to check the tasks of the service
+	local current_cluster_ips=""
+	while true; do
+		local auto_join_config=""
+		local cluster_ips=$(dockerswarm_sd "${1}")
+		# Skip if the cluster_ips is empty
+		if [[ -z "${cluster_ips}" ]]; then
+			current_cluster_ips="" # reset the current_cluster_ips
+			continue
+		fi
+		# if cluster_ips only contains one IP address, then it's a single node cluster
+		if [[ $(echo "${cluster_ips}" | wc -l) -eq 1 ]]; then
+			# Write the configuration to the file
+			echo "storage \"raft\" { /* single node cluster */ }" > "$VAULT_STORAGE_CONFIG_FILE"
+			continue
+		fi
+		# Check if the current_cluster_ips is different from the cluster_ips
+		if [[ "${current_cluster_ips}" != "${cluster_ips}" ]]; then
+			# Update the current_cluster_ips
+			current_cluster_ips=$cluster_ips
+			# Loop to add the tasks to the auto_join_config
+			for task in ${cluster_ips}; do
+				# Skip if the task is the current node
+				if [[ "$(hostname -i)" == *"${task}"* ]]; then
+					continue
+				fi
+				# Add the task to the auto_join_config
+				if [[ -n "${auto_join_config}" ]]; then
+					auto_join_config="${auto_join_config} "
+				fi
+				auto_join_config="${auto_join_config}retry_join { leader_api_addr = \"${auto_join_scheme}://${task}:${auto_join_port}\" }"
+			done
+			# Write the configuration to the file
+			echo "storage \"raft\" { ${auto_join_config} }" > "$VAULT_STORAGE_CONFIG_FILE"
+			# Send a SIGHUP signal to reload the configuration
+			if [ -f "$VAULT_PID_FILE" ]; then
+				echo "==> [Docker Swarm Entrypoint] detected a change in the cluster, reloading the configuration..."
+				kill -s SIGHUP $(cat $VAULT_PID_FILE)
+			fi
+		fi
+		sleep 15
+	done
 }
 
 # VAULT_DATA_DIR is exposed as a volume for possible persistent storage. The
@@ -132,45 +132,45 @@ sleep ${DOCKERSWARM_STARTUP_DELAY}
 
 # Auto-join the Docker Swarm service
 if [[ -n "${DOCKERSWARM_SERVICE_NAME}" ]]; then
-    echo "==> [Docker Swarm Entrypoint] configure auto-join for \"${DOCKERSWARM_SERVICE_NAME}\" stack..."
-    dockerswarm_auto_join $DOCKERSWARM_SERVICE_NAME &
+	echo "==> [Docker Swarm Entrypoint] configure auto-join for \"${DOCKERSWARM_SERVICE_NAME}\" stack..."
+	dockerswarm_auto_join $DOCKERSWARM_SERVICE_NAME &
 else
-    echo "==> [Docker Swarm Entrypoint] failed to configure auto-join: DOCKERSWARM_SERVICE_NAME is not set"
-    exit 1
+	echo "==> [Docker Swarm Entrypoint] failed to configure auto-join: DOCKERSWARM_SERVICE_NAME is not set"
+	exit 1
 fi
 
 # Generate a random node ID which will be persisted in the data directory
 if [ ! -f "${VAULT_DATA_DIR}/node-id" ]; then
-    echo "==> [Docker Swarm Entrypoint] generate a random node ID which will be persisted in the data directory..."
-    uuidgen > "${VAULT_DATA_DIR}/node-id"
+	echo "==> [Docker Swarm Entrypoint] generate a random node ID which will be persisted in the data directory..."
+	uuidgen > "${VAULT_DATA_DIR}/node-id"
 fi
 # Set the VAULT_RAFT_NODE_ID to the content of the node-id file
 export VAULT_RAFT_NODE_ID=$(cat "${VAULT_DATA_DIR}/node-id")
 
 # Set the VAULT_*_ADDR using VAULT_*_NETWORK
 if [ -n "$VAULT_API_NETWORK" ]; then
-    export VAULT_API_ADDR=$(dockerswarm_get_addr $VAULT_API_NETWORK ${VAULT_API_ADDR:-"https://0.0.0.0:8200"})
-    echo "==> [Docker Swarm Entrypoint] Using \"$VAULT_API_NETWORK\" network for VAULT_API_ADDR: $VAULT_API_ADDR"
+	export VAULT_API_ADDR=$(dockerswarm_get_addr $VAULT_API_NETWORK ${VAULT_API_ADDR:-"https://0.0.0.0:8200"})
+	echo "==> [Docker Swarm Entrypoint] Using \"$VAULT_API_NETWORK\" network for VAULT_API_ADDR: $VAULT_API_ADDR"
 fi
 if [ -n "$VAULT_REDIRECT_NETWORK" ]; then
-    export VAULT_REDIRECT_ADDR=$(dockerswarm_get_addr $VAULT_REDIRECT_NETWORK ${VAULT_REDIRECT_ADDR:-"http://0.0.0.0:8200"})
-    echo "==> [Docker Swarm Entrypoint] Using \"$VAULT_REDIRECT_NETWORK\" network for VAULT_REDIRECT_ADDR: $VAULT_REDIRECT_ADDR"
+	export VAULT_REDIRECT_ADDR=$(dockerswarm_get_addr $VAULT_REDIRECT_NETWORK ${VAULT_REDIRECT_ADDR:-"http://0.0.0.0:8200"})
+	echo "==> [Docker Swarm Entrypoint] Using \"$VAULT_REDIRECT_NETWORK\" network for VAULT_REDIRECT_ADDR: $VAULT_REDIRECT_ADDR"
 fi
 if [ -n "$VAULT_ADVERTISE_NETWORK" ]; then
-    export VAULT_ADVERTISE_ADDR=$(dockerswarm_get_addr $VAULT_ADVERTISE_NETWORK ${VAULT_ADVERTISE_ADDR:-"http://0.0.0.0:8200"})
-    echo "==> [Docker Swarm Entrypoint] Using \"$VAULT_ADVERTISE_NETWORK\" network for VAULT_ADVERTISE_ADDR: $VAULT_ADVERTISE_ADDR"
+	export VAULT_ADVERTISE_ADDR=$(dockerswarm_get_addr $VAULT_ADVERTISE_NETWORK ${VAULT_ADVERTISE_ADDR:-"http://0.0.0.0:8200"})
+	echo "==> [Docker Swarm Entrypoint] Using \"$VAULT_ADVERTISE_NETWORK\" network for VAULT_ADVERTISE_ADDR: $VAULT_ADVERTISE_ADDR"
 fi
 if [ -n "$VAULT_CLUSTER_NETWORK" ]; then
-    export VAULT_CLUSTER_ADDR=$(dockerswarm_get_addr $VAULT_CLUSTER_NETWORK ${VAULT_CLUSTER_ADDR:-"https://0.0.0.0:8201"})
-    echo "==> [Docker Swarm Entrypoint] Using \"$VAULT_CLUSTER_NETWORK\" network for VAULT_CLUSTER_ADDR: $VAULT_CLUSTER_ADDR"
+	export VAULT_CLUSTER_ADDR=$(dockerswarm_get_addr $VAULT_CLUSTER_NETWORK ${VAULT_CLUSTER_ADDR:-"https://0.0.0.0:8201"})
+	echo "==> [Docker Swarm Entrypoint] Using \"$VAULT_CLUSTER_NETWORK\" network for VAULT_CLUSTER_ADDR: $VAULT_CLUSTER_ADDR"
 fi
 
 # If DOCKERSWARM_ENTRYPOINT is set, wait for the storage configuration file to be created
 if [[ -n "${DOCKERSWARM_ENTRYPOINT}" ]]; then
-    echo "==> [Docker Swarm Entrypoint] waiting for auto-join config \"$VAULT_STORAGE_CONFIG_FILE\" to be created..."
-    while [ ! -f "$VAULT_STORAGE_CONFIG_FILE" ]; do
-        sleep 1
-    done
+	echo "==> [Docker Swarm Entrypoint] waiting for auto-join config \"$VAULT_STORAGE_CONFIG_FILE\" to be created..."
+	while [ ! -f "$VAULT_STORAGE_CONFIG_FILE" ]; do
+		sleep 1
+	done
 fi
 
 # Redirect the execution context to the original entrypoint, if needed
